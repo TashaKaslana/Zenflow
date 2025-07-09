7ac395ee90279ba0481741a6d28f20b2e5be8fa9
Phong <sanhok2104@gmail.com>
2025-07-09 21:55:45 +0700

    feat: Add API response classes for error handling and pagination

---
+++ src/main/java/org/phong/zenflow/core/enums/SystemError.java
@@ -0,0 +1,25 @@
+package org.phong.zenflow.core.enums;
+
+
+import lombok.Getter;
+
+@Getter
+public enum SystemError {
+    VALIDATION_FAILED_MSG("system.validation_failed"),
+    MALFORMED_REQUEST_MSG("system.malformed_request"),
+    METHOD_NOT_SUPPORTED_MSG("system.method_not_supported"),
+    GENERIC_ERROR_MSG("system.generic_error"),
+    MISSING_PARAMETER_MSG("system.missing_parameter"),
+    DATA_INTEGRITY_VIOLATION("system.data_integrity_violation"),
+    INVALID_ARGUMENT_TYPE_MSG("system.invalid_argument_type"),
+    AUTHENTICATION_FAILED("system.authentication_failed"),
+    ACCESS_DENIED("system.access_denied"),
+    NOT_FOUND_ENDPOINT("system.not_found_endpoint"),
+    ;
+
+    private final String messageKey;
+
+    SystemError(String messageKey) {
+        this.messageKey = messageKey;
+    }
+}
---
+++ src/main/java/org/phong/zenflow/core/responses/ApiErrorResponse.java
@@ -0,0 +1,53 @@
+package org.phong.zenflow.core.responses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.phong.zenflow.core.enums.SystemError;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiErrorResponse {
+    private final int status;
+    private final String message;
+    private final String path;
+    private LocalDateTime timestamp = LocalDateTime.now();
+    private String error;
+    private Map<String, String> fieldErrors; // For validation errors specifically
+    private List<String> globalErrors; // For global validation errors
+
+    public ApiErrorResponse(int status, SystemError message, String path, String error) {
+        this.status = status;
+        this.message = message.getMessageKey();
+        this.path = path;
+        this.error = error;
+    }
+
+    public ApiErrorResponse(int status, String message, String path, String error) {
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.error = error;
+    }
+
+    public ApiErrorResponse(int status, SystemError message, String path, String error, Map<String, String> fieldErrors, List<String> globalErrors) {
+        this(status, message, path, error);
+        this.fieldErrors = fieldErrors;
+        this.globalErrors = globalErrors;
+    }
+
+    public ApiErrorResponse(int status, String message, String path, String error, Map<String, String> fieldErrors, List<String> globalErrors) {
+        this(status, message, path, error);
+        this.fieldErrors = fieldErrors;
+        this.globalErrors = globalErrors;
+    }
+}
