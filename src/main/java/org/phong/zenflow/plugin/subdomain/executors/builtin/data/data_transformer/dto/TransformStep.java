ca538d91d755b7bfeeb2ccb7124e41edc823a658
Phong <sanhok2104@gmail.com>
2025-08-12 11:56:51 +0700

    feat: add Mockito support for unit testing and enhance WorkflowConfig structure. fix: serialize issue from TransformStep by adding @NoArgsConstructor

--- src/main/java/org/phong/zenflow/plugin/subdomain/executors/builtin/data/data_transformer/dto/TransformStep.java
+++ src/main/java/org/phong/zenflow/plugin/subdomain/executors/builtin/data/data_transformer/dto/TransformStep.java
@@ -2,11 +2,13 @@ package org.phong.zenflow.plugin.subdomain.executors.builtin.data.data_transform
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransformStep {
     private String transformer;
     private Map<String, Object> params;
