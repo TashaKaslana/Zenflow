2d720360900ebb71cddea27f29a4868f981e3dda
Phong <sanhok2104@gmail.com>
2025-08-21 21:28:27 +0700

    feat: implement a logging framework with ExecutionContext, LogContext, and NodeLogPublisher classes

---
+++ src/main/java/org/phong/zenflow/workflow/subdomain/node_logs/logging/ExecutionContext.java
@@ -0,0 +1,37 @@
+package org.phong.zenflow.workflow.subdomain.node_logs.logging;
+
+import lombok.Getter;
+import org.phong.zenflow.workflow.subdomain.node_logs.service.NodeLogPublisher;
+
+import java.util.UUID;
+
+@Getter
+public class ExecutionContext {
+    private final UUID workflowId;
+    private final UUID workflowRunId;
+    private final String nodeKey;
+    private final NodeLogPublisher log;
+
+    public ExecutionContext(UUID workflowId, UUID workflowRunId, String nodeKey) {
+        this.workflowId = workflowId;
+        this.workflowRunId = workflowRunId;
+        this.nodeKey = nodeKey;
+        this.log = NodeLogPublisher.builder()
+                .workflowId(workflowId)
+                .workflowRunId(workflowRunId)
+                .nodeKey(nodeKey)
+                .build();
+    }
+
+    public ExecutionContext(UUID workflowId, UUID workflowRunId, String nodeKey, UUID userId) {
+        this.workflowId = workflowId;
+        this.workflowRunId = workflowRunId;
+        this.nodeKey = nodeKey;
+        this.log = NodeLogPublisher.builder()
+                .workflowId(workflowId)
+                .workflowRunId(workflowRunId)
+                .nodeKey(nodeKey)
+                .userId(userId)
+                .build();
+    }
+}
